5d4488b7c935f806806ff6b37517bd64784dbb7b
BuildTools <unconfigured@null.spigotmc.org>
2022-11-03 17:45:53 +0530

    keygen

--- generate/views.py
+++ generate/views.py
@@ -1,18 +1,14 @@
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 import json
+import uuid
 # from .models import Poll
 
 def upload(request):
     data = {"msg" : "hello!"}
     return JsonResponse(data)
-    # MAX_OBJECTS = 20
-    # polls = Poll.objects.all()[:MAX_OBJECTS]
-    # data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
-    # return JsonResponse(data)
-
 
 @csrf_exempt
 @require_POST
@@ -31,3 +27,9 @@ def generate(request):
             "message" : "file, token or size field not found"
             }
         return JsonResponse(error)
+
+@require_GET
+def keygen(request):
+    api_token = uuid.uuid4()
+    vla = {"token":f"{api_token}"}
+    return JsonResponse(vla)
