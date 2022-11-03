4ac10d5196597a636942879d991d8eb3788b8e47
K-Krishan <kkrishna.7065@gmail.com>
2022-11-03 16:25:35 +0530

    changes - 1D

--- generate/models.py
+++ generate/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Token:
+    token = models.CharField(max_length=256, primary_key=True)
+    data = models.IntegerField()
+    exp = models.DateField()
+
--- generate/views.py
+++ generate/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+from models import Token
 # Create your views here.
