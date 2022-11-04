31e5e616ec13bbee4e56cddf533f5b475bd3e2ce
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 18:30:09 +0530

    Update file handler

--- generate/crypt.py
+++ generate/crypt.py
@@ -1,45 +1,50 @@
 from cryptography.fernet import Fernet
-from subprocess import Popen
 import binascii
 import base64
 import mmap
 import os
 
 
+uploads_path = os.path.dirname(__file__) + '\\..\\Uploads\\'
 dir_path = os.path.dirname(__file__) + '\\..\\Uploads\\Temp\\'
+enc_dir_path = os.path.dirname(__file__) + '\\..\\Uploads\\Encrypted\\'
+
 key = base64.urlsafe_b64encode(b'00000000000000000000000000000000')
 large =  False
 MAX = 12000
 
-def encryptBlock(m):
-    f = Fernet(key)
-    if large:
-        while m.tell() != m.size():
-            if m.size() - m.tell() < MAX:
-                block = m.read(m.size() - m.tell())
-            else:
-                block = m.read(MAX)
-            random_data = binascii.hexlify(f.encrypt(block))
-            file_handler = os.path.dirname(__file__) + "\\file_handler.py"
-            Popen(["python", file_handler, random_data, ".enc", file])
-            m.seek(MAX+1)
+def makeDir(path_name):
+    if not os.path.exists(path_name):
+        os.mkdir(path_name)
+
+def storeFile(data, file):
+    makeDir(uploads_path)
+    makeDir(dir_path)
+    makeDir(enc_dir_path)
+
+    file_name = file.split('.')[0]
+    file_path = enc_dir_path + file_name + ".enc"
+    mode = ""
+
+    if not os.path.isfile(file_path):
+        mode = "wb"
+    else:
+        mode = "ab"
+
+    with open(file_path, mode) as f:
+        f.write(data)
     
+
+def encryptBlock(m, file):
+    f = Fernet(key)
     data = m.read(m.size())
     random_data = binascii.hexlify(f.encrypt(data))
-    file_handler = os.path.dirname(__file__) + "\\file_handler.py"
-    Popen(["python", file_handler, random_data, ".enc", file])
+    storeFile(random_data, file)
 
 
 files = os.listdir(dir_path)
 for file in files:
     f = open(dir_path+file, 'r+b')
     m = mmap.mmap(f.fileno(), 0)
-    size = m.size()
-    if size > MAX:
-        large = True
-    else:
-        large = False
-    
-    data = m.read(size)
     f.close()
-    encryptBlock(m)
+    encryptBlock(m, file)
--- generate/views.py
+++ generate/views.py
@@ -2,9 +2,11 @@ from django.views.decorators.http import require_POST, require_GET
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
+from threading import Thread
 import json
 import uuid
-# from .models import Poll
+import time
+import os
 
 def upload(request):
     data = {"msg" : "hello!"}
@@ -33,3 +35,28 @@ def keygen(request):
     api_token = uuid.uuid4()
     vla = {"token":f"{api_token}"}
     return JsonResponse(vla)
+
+def makeDir(path_name):
+    if not os.path.exists(path_name):
+        os.mkdir(path_name)
+
+def writeFile(data):
+    uploads_path = os.path.dirname(__file__) + '\\..\\Uploads\\'
+    dir_path = os.path.dirname(__file__) + '\\..\\Uploads\\Temp\\'
+    enc_dir_path = os.path.dirname(__file__) + '\\..\\Uploads\\Encrypted\\'
+    makeDir(uploads_path)
+    makeDir(dir_path)
+    makeDir(enc_dir_path)
+
+    file_name = time.strftime("%Y%m%d-%H%M%S%p")
+    file_path = dir_path + file_name + type
+
+    mode = ""
+
+    if not os.path.isfile(file_path):
+        mode = "wb"
+    else:
+        mode = "ab"
+
+    with open(file_path, mode) as f:
+        f.write(data.encode())
