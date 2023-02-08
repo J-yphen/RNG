b763c676cc9a6b13b7a1c24d0b1159d2ed7b602b
Jay <17102292j@gmail.com>
2023-02-08 23:46:52 +0530

    User balance & validaty info using API

--- generate/urls.py
+++ generate/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import generate, upload, form
+from .views import generate, user_quota, upload, form
 
 urlpatterns = [
     path("api/generate/", generate, name="Random number generator"),
+    path("api/balance/", user_quota, name="User Quota"),
     path("token/", form, name="token generation"),
     path("", upload, name="upload"),
 ]
